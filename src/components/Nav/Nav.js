31d837496b97dd226e6f5f389ff466240fb68a41
Filippo Piggici <filippo.piggici@treatwell.com>
2021-01-15 11:16:50 +0100

    refactor and improve theme activation handling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,26 +1,11 @@
 import React, {useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faListOl, faMoon, faMusic, faRainbow, faSun} from "@fortawesome/free-solid-svg-icons";
+import toggleTheme from "../../utils/handleTheme";
 import styles from './Nav.module.css';
 
 export default function Nav({setLibraryStatus}) {
   const [themeWindowOpen, setThemeWindowOpen] = useState(false);
-
-  function toggleTheme(style) {
-    switch (style) {
-      case "dark":
-        enableDarkModeStyle();
-        break;
-      case "light":
-        enableLightModeStyle();
-        break;
-      case "gradient":
-        enableGradientModeStyle();
-        break;
-      default:
-        enableLightModeStyle();
-    }
-  }
 
   return (
     <nav>
@@ -51,31 +36,4 @@ export default function Nav({setLibraryStatus}) {
       </div>
     </nav>
   )
-}
-
-function enableDarkModeStyle() {
-  document.documentElement.style
-    .setProperty('--main-color-background', 'rgb(52, 52, 52)');
-  document.documentElement.style
-    .setProperty('--main-color-font', 'rgb(255, 255, 255)');
-  document.documentElement.style
-    .setProperty('--main-color-selected', 'rgb(31,94,108)');
-}
-
-function enableLightModeStyle() {
-  document.documentElement.style
-    .setProperty('--main-color-background', 'rgb(255, 255, 255)');
-  document.documentElement.style
-    .setProperty('--main-color-font', 'rgb(52, 52, 52)');
-  document.documentElement.style
-    .setProperty('--main-color-selected', 'rgba(173, 216, 230, 1)');
-}
-
-function enableGradientModeStyle() {
-  document.documentElement.style
-    .setProperty('--main-color-background', 'linear-gradient(to left, #355c7d, #6c5b7b, #c06c84)');
-  document.documentElement.style
-    .setProperty('--main-color-font', 'rgb(255, 255, 255)');
-  document.documentElement.style
-    .setProperty('--main-color-selected', 'rgb(104, 86, 122)');
 }
---
+++ src/utils/handleTheme.js
@@ -0,0 +1,34 @@
+export default function toggleTheme(style) {
+
+  const GREY = 'rgb(52, 52, 52)';
+  const WHITE = 'rgb(255, 255, 255)';
+  const GREEN = 'rgb(31,94,108)';
+  const LIGHT_BLUE = 'rgb(173,216,230)';
+  const PURPLE = 'rgb(104, 86, 122)';
+  const GRADIENT = 'linear-gradient(to left, #355c7d, #6c5b7b, #c06c84)';
+
+  switch (style) {
+    case "dark":
+      activateThemeStyle(GREY, WHITE, GREEN);
+      break;
+    case "light":
+      activateThemeStyle(WHITE, GREY, LIGHT_BLUE);
+      break;
+    case "gradient":
+      activateThemeStyle(GRADIENT, WHITE, PURPLE);
+      break;
+    default:
+      // light theme
+      activateThemeStyle(WHITE, GREY, LIGHT_BLUE);
+  }
+}
+
+
+function activateThemeStyle(backgroundColor, fontColor, selectionColor) {
+  document.documentElement.style
+    .setProperty('--main-color-background', backgroundColor);
+  document.documentElement.style
+    .setProperty('--main-color-font', fontColor);
+  document.documentElement.style
+    .setProperty('--main-color-selected', selectionColor);
+}
